fa458ce1ec5d031b785fc355630db194d842b757
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2024-11-08 21:28:31 +0000

    feat(google_storage_bucket): create mlflow bucket for model artifact
    bucket created for model artifact, added new environment variables `env_prefix` and `project_name`

--- main.tf
+++ main.tf
@@ -1,2 +1,22 @@
 # Data source to get project details
 data "google_project" "project" {}
+
+locals {
+  name_prefix = "${var.project_name}-${var.gcp_region}-${var.env_prefix}"
+}
+
+
+#---------------------------------------------------
+# Bucket
+#---------------------------------------------------
+resource "google_storage_bucket" "mlflow_bucket" {
+  name                        = "${local.name_prefix}-mlflow-bucket"
+  location                    = "EU"
+  storage_class               = "STANDARD"
+  force_destroy               = true
+  uniform_bucket_level_access = true
+  public_access_prevention    = "enforced"
+  versioning {
+    enabled = true
+  }
+}
--- variables.tf
+++ variables.tf
@@ -1,6 +1,20 @@
+variable "env_prefix" {
+  description = <<-EOF
+  The prefix added to resources in the environment.
+
+EOF
+
+  type = string
+  validation {
+    condition     = contains(["dev", "staging", "prod", "sandbox"], var.env_prefix)
+    error_message = "The env_prefix value must be either: dev, staging, prod or sandbox."
+  }
+}
+
 variable "gcp_default_labels" {
   description = <<-EOF
-  The default region to manage resources in.
+  Labels that will be applied to all resources with a top level labels field or a labels
+  field nested inside a top level metadata field. 
 
 EOF
 
@@ -30,6 +44,15 @@ variable "gcp_zone" {
   description = <<-EOF
   The default zone to manage resources in. Generally, 
   this zone should be within the default region you specified.
+
+EOF
+
+  type = string
+}
+
+variable "project_name" {
+  description = <<-EOF
+  The name of the project.
 
 EOF
 
