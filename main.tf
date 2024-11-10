ddfa0d01b3aa602c2ffe547a351b797ab6506b64
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2024-11-10 10:29:54 +0000

    feat(service_accounts): create prediction and github action service accounts
    service accounts for github actions to push docker images to artifact registry and prediction service account to get objects from bucket(s)

--- main.tf
+++ main.tf
@@ -3,12 +3,17 @@ data "google_project" "project" {}
 
 locals {
   name_prefix = "${var.project_name}-${var.gcp_region}-${var.env_prefix}"
+  github_actions_roles = [
+    "roles/artifactregistry.writer",
+    "roles/iam.serviceAccountTokenCreator",
+  ]
 }
 
 
 #---------------------------------------------------
 # Bucket
 #---------------------------------------------------
+#trivy:ignore:AVD-GCP-0066
 resource "google_storage_bucket" "mlflow_bucket" {
   name                        = "${local.name_prefix}-mlflow-bucket"
   location                    = "EU"
@@ -16,7 +21,81 @@ resource "google_storage_bucket" "mlflow_bucket" {
   force_destroy               = true
   uniform_bucket_level_access = true
   public_access_prevention    = "enforced"
+  project                     = data.google_project.project.number
   versioning {
     enabled = true
   }
+}
+
+#---------------------------------------------------
+# Registry
+#---------------------------------------------------
+resource "google_artifact_registry_repository" "prediction_service_registry" {
+  location      = var.gcp_region
+  repository_id = "${var.env_prefix}-prediction-service"
+  description   = "Docker registry for prediction microservice"
+  format        = "DOCKER"
+  # project       = data.google_project.project.id
+
+
+  docker_config {
+    immutable_tags = true
+  }
+}
+
+#---------------------------------------------------
+# Service Accounts
+#---------------------------------------------------
+resource "google_service_account" "prediction_service_account" {
+  account_id   = "${var.env_prefix}-prediction-service-account"
+  display_name = "Prediction Service Account"
+  description  = "The service account needed to download from mlflow bucket"
+}
+
+resource "google_service_account" "github_actions_service_account" {
+  count        = var.env_prefix == "dev" ? 1 : 0
+  account_id   = "${var.env_prefix}-github-act-service-account"
+  display_name = "GitHub Actions Service Account"
+  description  = "The service account needed for GitHub actions workflows"
+}
+
+#---------------------------------------------------
+# Service Accounts Keys
+#---------------------------------------------------
+resource "google_service_account_key" "github_actions_service_account_key" {
+  count              = var.env_prefix == "dev" ? 1 : 0
+  service_account_id = google_service_account.github_actions_service_account[0].name
+  private_key_type   = "TYPE_GOOGLE_CREDENTIALS_FILE"
+}
+
+resource "google_service_account_key" "prediction_service_account_key" {
+  service_account_id = google_service_account.prediction_service_account.name
+  private_key_type   = "TYPE_GOOGLE_CREDENTIALS_FILE"
+}
+
+
+#---------------------------------------------------
+# IAM Policy Bindings
+#---------------------------------------------------
+resource "google_project_iam_binding" "github_actions_registry_role" {
+  count = var.env_prefix == "dev" ? length(local.github_actions_roles) : 0
+
+  project = data.google_project.project.number
+  role    = local.github_actions_roles[count.index]
+  members = [
+    "serviceAccount:${google_service_account.github_actions_service_account[0].account_id}@${data.google_project.project.project_id}.iam.gserviceaccount.com"
+  ]
+
+  depends_on = [google_service_account.github_actions_service_account]
+}
+
+
+resource "google_project_iam_binding" "prediction_service_account_role" {
+  project = data.google_project.project.number
+  role    = "roles/storage.objectViewer"
+  members = [
+    "serviceAccount:${google_service_account.prediction_service_account.account_id}@${data.google_project.project.project_id}.iam.gserviceaccount.com"
+  ]
+
+  depends_on = [google_service_account.prediction_service_account]
 }
--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,37 @@ EOF
 
   value = data.google_project.project.number
 }
+
+output "github_action_service_account_key" {
+  description = <<-EOF
+    The GitHub service account private key in JSON format, base64 encoded.
+EOF
+
+  value     = google_service_account_key.github_actions_service_account_key[0].private_key
+  sensitive = true
+}
+
+output "prediction_service_account_key" {
+  description = <<-EOF
+    The prediction service private key in JSON format, base64 encoded.
+EOF
+
+  value     = google_service_account_key.prediction_service_account_key.private_key
+  sensitive = true
+}
+
+output "predicition_service_docker_registry_id" {
+  description = <<-EOF
+    The prediction service docker registry ID.
+EOF
+
+  value = google_artifact_registry_repository.prediction_service_registry.id
+}
+
+output "predicition_service_docker_registry_name" {
+  description = <<-EOF
+    The prediction service docker registry name.
+EOF
+
+  value = google_artifact_registry_repository.prediction_service_registry.name
+}
